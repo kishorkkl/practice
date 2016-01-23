416ffc9251c04b8db7b78fe35b2056a2fdf1f85e
sandeep <sandeep@b8ba2004-9af9-421d-82ba-01e6b6663f6e>
2016-01-23 17:15:00 +0000

    Modified by Nikhil/Himanshu on 23 jan

--- src/main/java/com/jse7p1/javabasics/TestCalendar.java
+++ src/main/java/com/jse7p1/javabasics/TestCalendar.java
@@ -25,5 +25,171 @@ package com.jse7p1.javabasics;
        g) You should also make questions that dont have code and test specific knowledge of the topic but no more than 2 per topic and should test important aspect of the question. These should be in addition to 5 code related questions.
 
  */
-public class TestCalendar {
+/*public class TestCalendar {
+}*/
+
+
+
+  /*Testing Topic 1: Java applications with a main method
+
+    Q1.What will be the output of the code below?
+
+        Answers:
+
+        a)null
+        b)Compile time Error.
+        c)TestCalendar program Running
+        d)Run time Error
+
+
+        Correct Answer:
+        c) TestCalendar program Running.
+   */
+
+/*
+   public class TestCalendar{
+    public static void main(String[] args)
+    {
+        if (null==null) {
+            System.out.println("TestCalendar program Running");
+        }
+    }
 }
+*/
+
+
+  /* Q2.What will be the output of the code below?
+
+        Answers:
+
+        a)time is true
+        b)Compile time Error.
+        c)time is false.
+        d)Java.lang.ArithmeticException
+
+
+        Correct Answer:
+        a)time is true.
+        */
+
+ /* public class TestCalendar{
+    public static void main(String[] args)
+    {
+        boolean time = false;
+        if(time=true){
+        System.out.println("time is true");
+        }else{
+        System.out.println("time is false");
+        }
+        }
+    }
+    */
+
+/* Q3.What will be the output of the code below?
+
+        Answers:
+
+        a)true
+          true
+        b)false
+          true
+        c)true
+          false
+        d)false
+          false
+
+
+        Correct Answer:
+        b)false
+          true
+
+     *Tip:--Integer Limit 0-127
+        */
+
+
+  /*
+  public class TestCalendar{
+    public static void main(String[] args) {
+
+        Integer min = 128;
+
+        Integer sec = 128;
+
+        System.out.println(min == sec);
+
+        Integer minute = 127;
+        Integer second = 127;
+
+        System.out.println(minute == second);
+        }
+    }
+    */
+
+/* Q4.What will be the output of the code below?
+
+        Answers:
+
+        a)Value of Integer:null
+          Value of String:null
+        b)Value of Integer:
+          Value of String:
+        c)Compile time Error :The constructor String(String) is ambiguous
+        d)Compile time Error :The constructor Integer(Integer) is ambiguous
+
+
+        Correct Answer:
+        b)false
+          true
+
+     *Tip:--Integer Limit 0-127
+        */
+
+
+/*
+  public class TestCalendar{
+    public static void main(String [] args)
+        {
+        Integer i = new Integer(null);
+        String s = new String(null);
+
+        System.out.println("Value of Integer:" +i);
+        System.out.println("Value of String:" +s);
+        }
+    }
+    */
+
+/* Q5.What will be the output of the code below?
+
+        Answers:
+
+        a)Compile Time Error
+        b)Run Time Error
+        c)Exception:java.lang.NullPointerException
+        d)Exception: java.lang.StackOverflowError
+
+
+        Correct Answer:
+        d)Exception: java.lang.StackOverflowError
+
+/*
+  public class TestCalendar{
+    TestCalendar test = new TestCalendar();
+    public static void main(String [] args)
+        {
+          TestCalendar test = new TestCalendar();
+          test.show();
+        }
+        public int show(){
+        return (true ? null: 0);
+        }
+    }
+    */
+
+
+
+
+
+
+
+
+
