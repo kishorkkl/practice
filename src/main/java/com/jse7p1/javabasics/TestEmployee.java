23fb2183d33ff55290a2c089f96846c5574c3da9
sandeep <sandeep@b8ba2004-9af9-421d-82ba-01e6b6663f6e>
2016-01-23 17:57:36 +0000

    modified by ramesh on 23 jan

--- src/main/java/com/jse7p1/javabasics/TestEmployee.java
+++ src/main/java/com/jse7p1/javabasics/TestEmployee.java
@@ -87,7 +87,7 @@ class Employee
     b) Employee class is  abstract and therefore cannot be instantiated.
 
  */
-
+/*
 public class TestEmployee {
     public static void main(String[] args)
     {
@@ -116,3 +116,201 @@ abstract class Employee
     String getEmployeeId();
 
 }
+*/
+
+/*
+    Q3 What will be the output of the code below?
+
+
+    Answers:
+    a) x=0 will be printed 6 times.
+    b)Compile time Error.
+    c)Run time Error
+    d) x=0 will be printed 5 times.
+
+    Correct Answer:
+
+    a) x=0 will be printed 6 times.
+
+ */
+/*
+public class TestEmployee {
+
+    public static void main(String[] args) {
+        int x;
+        for (x=0;x<=5;x++)
+        {
+            int y=0;
+            System.out.println("x = "+y);
+        }
+
+    }
+
+}
+*/
+
+/*
+    Q4 What will be the output of the code below?
+
+
+    Answers:
+    a) employye = 1, employe = 3
+    b)Compile time Error.
+    c)Run time Error
+
+
+    Correct Answer:
+
+    b) employe cannot be resolved a variable.
+
+ */
+/*
+public class TestEmployee {
+    public static void main(String[] args) {
+        int employee = 1;
+
+        {
+            int employe = 2;
+            System.out.println("employye = " + employee);
+        }
+
+        int employye = 3;
+        System.out.println("employe = " + employe);
+    }
+}
+*/
+
+/*
+Q5 What will be the output of the code below?
+
+
+        Answers:
+        a) age:20
+        b)Compile time Error.
+        c)Run time Error
+
+
+        Correct Answer:
+
+        b) age cannot be resolved to a variable
+
+        // System.out.println("Age: " + age); should be written in Employeeage().
+
+ */
+
+/*
+public class TestEmployee {
+    public void Employeeage() {
+        int age = 0;
+        age = age + 20;
+
+    }
+
+    public static void main(String args[]) {
+        TestEmployee te = new TestEmployee();
+        te.Employeeage();
+        System.out.println("Age: " + age);
+    }
+}
+
+*/
+/*
+Q6 What will be the output of the code below?
+
+
+        Answers:
+        a) Obj1: count is=2,Obj2: count is=3
+        b)Compile time Error.
+        c)Run time Error
+        d) Obj1: count is=2,Obj2: count is=2
+
+
+
+        Correct Answer:
+
+        d) Static variable shared by multiple objects
+
+ */
+
+/*
+public class TestEmployee
+{
+    static int count=0;
+    public void increment()
+    {
+        count++;
+    }
+    public static void main(String args[])
+    {
+        TestEmployee obj1=new TestEmployee();
+        TestEmployee obj2=new TestEmployee();
+        obj1.increment();
+        obj2.increment();
+        System.out.println("Obj1: count is="+obj1.count);
+        System.out.println("Obj2: count is="+obj2.count);
+    }
+}
+*/
+
+/*
+Q7 What will be the output of the code below?
+
+
+        Answers:
+        a) I'm a static variable
+        b)Compile time Error.
+        c)Run time Error
+        d) Obj1: count is=2, Obj2: count is=2
+
+
+
+        Correct Answer:
+
+        b) name cannot be resolved or is not a field
+
+ */
+
+/*
+public class TestEmployee {
+    public static void main(String[] args) {
+        System.out.println(Employee.name);
+    }
+
+
+    public class Employee {
+        public static final String NAME = "I'm a static variable";
+    }
+}
+
+*/
+
+/*
+Q8 If you are declaring class variable as public static final, then variable(constant) should be:
+
+
+        Answers:
+        a) in lower case
+        b) in upper case
+        c)does not matter
+
+        Correct Answer:
+
+        b) variable(constant) should be in upper case.
+
+ */
+
+/*
+Q9 Static variable’s value is same for all the objects(or instances) of the class:
+
+
+        Answers:
+        a) yes
+        b) no
+        c) none of above
+
+        Correct Answer:
+
+        a) yes, instances(objects) of the same class share a single copy of static variables.
+
+ */
+
