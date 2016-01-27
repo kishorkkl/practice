37b7da2b1b262605db286c002a67b658ecfa99be
sandeep <sandeep@b8ba2004-9af9-421d-82ba-01e6b6663f6e>
2016-01-27 23:20:41 +0000

    Rejected commit: Default

---
+++ Practice/src/main/java/com/jse7p1/datatypes/TestEmployee.java
@@ -0,0 +1,13 @@
+package com.jse7p1.datatypes;
+
+/**
+ * Created by sandeep on 27/01/2016.
+ *  * Exam Object: Working With Java Data Types
+ *
+ *   This class should Test the following
+ *   a) Declare and initialize variables
+ *   b) Differentiate between object reference variables and primitive variables
+ *   c) Read or write to object fields
+ */
+public class TestEmployee {
+}
