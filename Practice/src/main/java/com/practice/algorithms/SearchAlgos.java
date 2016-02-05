de334ab11398f181caed633f1688312196fab1e3
sandeep <sandeep@b8ba2004-9af9-421d-82ba-01e6b6663f6e>
2016-02-05 00:26:45 +0000

    Rejected commit: Default

--- Practice/src/main/java/com/practice/algorithms/SearchAlgos.java
+++ Practice/src/main/java/com/practice/algorithms/SearchAlgos.java
@@ -151,6 +151,63 @@ class SequentialSearchST<Key , Value>
     }
 }
 
+/*
+*  Symbol Table implementation usign Binary Search.
+ */
+
+class BinarySearchST<Key extends Comparable<Key>, Value>
+{
+    private Key[] keys;
+    private Value[] vals;
+    private int N;
+    public BinarySearchST(int capacity)
+    { // See Algorithm 1.1 for standard array-resizing code.
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
+    }
+    public int size()
+    { return N; }
+    public Value get(Key key)
+    {
+        if (isEmpty()) return null;
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) return vals[i];
+        else return null;
+    }
+    public int rank(Key key)
+    {
+        int lo = 0, hi = N-1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) hi = mid - 1;
+            else if (cmp > 0) lo = mid + 1;
+            else return mid;
+        }
+        return lo;
+    }
+
+    public void put(Key key, Value val)
+    { // Search for key. Update value if found; grow table if new.
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0)
+        { vals[i] = val; return; }
+        for (int j = N; j > i; j--)
+        { keys[j] = keys[j-1]; vals[j] = vals[j-1]; }
+        keys[i] = key; vals[i] = val;
+        N++;
+    }
+    public void delete(Key key)
+    {
+        //@TODO
+    }
+
+    private boolean isEmpty()
+    {
+        return false;
+    }
+}
 /**
  * TODO
  * 1. BST -> Binary search tree
